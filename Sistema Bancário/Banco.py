6c8d67d34778363798759cdfe06387bcfee7df94
Ulpio <ulpionetto0@gmail.com>
2023-06-06 22:46:25 -0300

    Versão 2.0, utilizando funções

--- Sistema Bancário/Banco.py
+++ Sistema Bancário/Banco.py
@@ -1,44 +1,57 @@
-print("Bem vindo ao Banco Oxetech, qual operação gostaria de realizar?")
+print("Bem vindo ao Banco Oxetech")
+print("Insira qual operação deseja realizar")
 print("1 - Saque")
 print("2 - Depósito")
 print("3 - Transferência")
 print("4 - Saldo")
 print("5 - Sair")
 
-saldo = 0
-while True:
-    opcao = int(input("Digite a opção desejada: "))
-    if opcao == 1:
-        print("Você escolheu a opção de saque.")
-        valor = float(input("Digite o valor que deseja sacar: "))
-        if valor <= saldo:
-            saldo = saldo - valor
-            print("Saque realizado com sucesso!")
-            print("Seu novo saldo é de: ", saldo)
-        else:
-            print("Saldo insuficiente.")
-    elif opcao == 2:
-        print("Você escolheu a opção de depósito.")
-        valor = float(input("Digite o valor que deseja depositar: "))
-        saldo = saldo + valor
-        print("Depósito realizado com sucesso!")
-        print("Seu novo saldo é de: ", saldo)
-    elif opcao == 3:
-        print("Você escolheu a opção de transferência.")
-        valor = float(input("Digite o valor que deseja transferir: "))
-        if valor <= saldo:
-            saldo = saldo - valor
-            print("Transferência realizada com sucesso!")
-            print("Seu novo saldo é de: ", saldo)
-        else:
-            print("Saldo insuficiente.")
-    elif opcao == 4:
-        print("Você escolheu a opção de saldo.")
-        print("Seu saldo é de: ", saldo)
-    elif opcao == 5:
-        print("Você escolheu a opção de sair.")
-        print("Até logo!")
-        break
-        quit()
+opcao = int(input("Digite a opção desejada: "))
+
+dinheiro = 0
+
+def Saque():
+    global dinheiro
+    saque = int(input("Digite o valor que deseja sacar: "))
+    if saque > dinheiro:
+        print("Saldo insuficiente")
     else:
-        print("Opção inválida.")
+        dinheiro = dinheiro - saque
+        print("Saque realizado com sucesso")
+        print("Seu saldo atual é de: ", dinheiro)
+
+def Deposito():
+    global dinheiro
+    deposito = int(input("Digite o valor que deseja depositar: "))
+    dinheiro = dinheiro + deposito
+    print("Depósito realizado com sucesso")
+    print("Seu saldo atual é de: ", dinheiro)
+
+def Transferencia():
+    global dinheiro
+    transferencia = int(input("Digite o valor que deseja transferir: "))
+    if transferencia > dinheiro:
+        print("Saldo insuficiente")
+    else:
+        dinheiro = dinheiro - transferencia
+        print("Transferência realizada com sucesso")
+        print("Seu saldo atual é de: ", dinheiro)
+        
+def Saldo():
+    global dinheiro
+    print("Seu saldo atual é de: ", dinheiro)
+
+while opcao != 5:
+    if opcao == 1:
+        Saque()
+    elif opcao == 2:
+        Deposito()
+    elif opcao == 3:
+        Transferencia()
+    elif opcao == 4:
+        Saldo()
+    else:
+        print("Opção inválida")
+    opcao = int(input("Digite a opção desejada: "))
+print("Obrigado por utilizar o Banco Oxetech")
+print("Volte sempre")
